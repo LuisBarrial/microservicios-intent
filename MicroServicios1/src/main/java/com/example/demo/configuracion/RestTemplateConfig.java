4fbf4b08cdd2a7990eee3565d8c23f2d3779f16c
LuisBarrial <angeloxz.com@gmail.com>
2023-06-09 04:29:27 -0500

    zipkinV2

--- MicroServicios1/src/main/java/com/example/demo/configuracion/RestTemplateConfig.java
+++ MicroServicios1/src/main/java/com/example/demo/configuracion/RestTemplateConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import feign.Capability;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
+
 @Configuration
 public class RestTemplateConfig {
 	
@@ -12,6 +16,10 @@ public class RestTemplateConfig {
 	@LoadBalanced
 	public RestTemplate restTemplate(){
 		return new RestTemplate(); 
+	}
+	@Bean
+	public Capability capability(final MeterRegistry registry) {
+	    return new MicrometerCapability(registry);
 	}
 
 }
